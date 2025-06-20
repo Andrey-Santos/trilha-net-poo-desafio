549d26cdafb19138dba6edf6fd1b0c15a6943dbd
Andrey Santos <andrey-carlos@outlook.com>
2025-06-20 18:35:19 -0300

    Feat: Criado classe base e implementado a mesma nas classes Iphone e nokia

--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Smartphone Nokia:");
+            Nokia nokia = new Nokia(numero: "123456789", modelo: "Nokia 3310", imei: "111111111111111", memoria: 64);
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("WhatsApp");
+            Console.WriteLine($"Número: {nokia.Numero}, Modelo: {nokia.Modelo}, IMEI: {nokia.Imei}, Memória: {nokia.Memoria}GB\n");
+
+            Console.WriteLine("Smartphone iPhone:");
+            Iphone iphone = new Iphone(numero: "987654321", modelo: "iPhone 15 Pro", imei: "222222222222222", memoria: 256);
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+            Console.WriteLine($"Número: {iphone.Numero}, Modelo: {iphone.Modelo}, IMEI: {iphone.Imei}, Memória: {iphone.Memoria}GB\n");
+        }
+    }
+}
